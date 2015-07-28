851d68c689c8ff753c730350d5fc825637b66bac
Ikey Benzaken <ikey.benz@gmail.com>
2015-07-28 13:21:09 -0400

    AMAN WAS HERE

---
+++ Enemy.swift
@@ -0,0 +1,34 @@
+//
+//  Enemy.swift
+//  HighJacked_AttemptThree
+//
+//  Created by Ikey Benzaken on 7/27/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
+
+
+protocol EnemyDelegate{
+    func enemyKilled(score: Int)
+}
+
+
+class Enemy: CCSprite {
+    var delegate: EnemyDelegate!
+    var isShooting: Bool = false
+    
+    func didLoadFromCCB() {
+        userInteractionEnabled = true
+    }
+    
+    override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        var randomScoreIncrease = arc4random_uniform(4) + 2
+        delegate.enemyKilled(Int(randomScoreIncrease))
+        isShooting = false
+        removeFromParent()
+    }
+    
+   
+    
+}
